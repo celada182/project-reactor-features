32ec8e7a8c90fa7aa86b59ce2a6e78dc32a3707d
Javier Celada <celada.jcm.javier@gmail.com>
2025-10-27 10:15:04 +0100

    Mono and Flux examples

--- src/main/java/com/celada/Main.java
+++ src/main/java/com/celada/Main.java
@@ -1,16 +1,36 @@
 package com.celada;
 
 import lombok.extern.java.Log;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Log
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        Mono<String> test = Mono.just("Hello")
-                .doOnNext(v -> log.info("[onNext]: " + v));
 
-        test.subscribe();
+        // Publisher
+        Mono<String> mono = Mono.just("Hello")
+                .doOnNext(v -> log.info("[onNext]: " + v))
+                .doOnSuccess(v -> log.info("[onSuccess]: " + v))
+                .doOnError(error -> log.info("[onError]: " + error.getMessage()));
+
+        // Consumer
+        mono.subscribe(
+                data -> log.info("Receiving Mono Data: " + data),
+                error -> log.info("Mono Error: " + error.getMessage()),
+                () -> log.info("Mono Success")
+        );
+
+        // Publisher
+        Flux<String> flux = Flux.just("Java", "Spring", "Reactor")
+                .doOnNext(v -> log.info("[onNext]: " + v))
+                .doOnComplete(() -> log.info("[onComplete]"));
+
+        // Consumer
+        flux.subscribe(
+                data -> log.info("Receiving Flux Data: " + data),
+                error -> log.info("Flux Error: " + error.getMessage()),
+                () -> log.info("Flux Success")
+        );
     }
 }
